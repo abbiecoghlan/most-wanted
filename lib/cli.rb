8781a81204a7849195f6c447cb5843dae05454fe
“Abbie <abbie.coghlan@gmail.com>
2021-01-06 09:52:16 -0600

    updated run and cli class

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,23 +1,34 @@
-PROMPT = TTY::Prompt.new
+class CLI 
 
-def greet
-    puts 'You are entering the FBI 50 most wanted database'
-end
+    def run
+        clear_screen
+        puts "You are entering the FBI's 50 most wanted database"
+    end 
 
-def main_menu
-    puts "please select from the following options"
-end
+    prompt = TTY::Prompt.new
+
+    def greet
+        puts 
+    end
+
+    def main_menu
+        puts "Please select from the following options"
+    end
 
 
-def run
-    greet 
-    puts "Enter a name to access a fugitive record:"
-    input = gets.chomp.upcase
-    # binding.pry
-    selection = Fugitive.find_by(name: input)
-    puts "Fugitive name: #{selection.name}"
-    puts "Fugitive age: #{selection.age}"
-    puts "Fugitive alias: #{selection.alias}"
+    def run
+        greet 
+        puts "Enter a name to access a fugitive record:"
+        input = gets.chomp.upcase
+        # binding.pry
+        selection = Fugitive.find_by(name: input)
+        puts "Fugitive name: #{selection.name}"
+        puts "Fugitive age: #{selection.age}"
+        puts "Fugitive alias: #{selection.alias}"
+    end 
+
+    def clear_screen
+        system "clear"
+    end
+
 end 
-
-
