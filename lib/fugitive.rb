a6f9ebbc19012f60fc41234091cede1bdf60fb41
Abbie <abbie.coghlan@gmail.com>
2021-01-05 18:54:59 -0600

    added methods to fugitives

--- lib/fugitive.rb
+++ lib/fugitive.rb
@@ -1,4 +1,24 @@
 class Fugitive < ActiveRecord::Base
     has_many :crimes
     has_many :cities, through: :crimes
+
+    def self.average_age
+        ages = self.select {|fugitive| fugitive.age != nil}.collect {|fugitive| fugitive.age}
+        ages.sum / ages.count
+    end
+
+    def self.oldest_criminal
+        self.select {|fugitive| fugitive.age != nil}.max_by {|fugitive| fugitive.age}
+    end
+
+    def self.youngest_criminal
+        self.select {|fugitive| fugitive.age != nil}.min_by {|fugitive| fugitive.age}
+    end
+
+    # def self.most_common_hair_color
+    #     hair_colors = self.select {|fugitive| fugitive.hair_color}
+    #     hair_colors.max_by {|color| hair_colors.count(color)}
+    # end 
+
+
 end
