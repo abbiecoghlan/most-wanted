5e09b4ef71c2993334afb66e13db1d1686a1a947
Abbie <abbie.coghlan@gmail.com>
2021-01-06 11:17:02 -0600

    cli methods added

--- lib/cli.rb
+++ lib/cli.rb
@@ -9,7 +9,7 @@ class CLI
     def main_menu
         prompt = TTY::Prompt.new
         menu = prompt.select("Please select from the following options:",%w(Access_records Update_records Create_new_records Delete_records))
-        case selection
+        case menu
         when "Access_records" 
             access_records
         when "Update_records"
@@ -22,18 +22,41 @@ class CLI
     end
 
     def access_records
+        clear_screen
         prompt = TTY::Prompt.new
         access_menu = prompt.select("Please select from the following options:",%w(Access_fugitive_data Access_crime_data Access_city_data Main_menu))
+        
+        case access_menu
+        when "Main_menu"
+            main_menu
+        end 
+
     end 
 
+   
     def update_records
+        clear_screen
+        prompt = TTY::Prompt.new
+        update_menu = prompt.select("Please select from the following options:",%w(Update_fugitive_data Update_crime_data Update_city_data Main_menu))
+
+        case update_menu
+        when "Main_menu"
+            main_menu
+        end 
+
     end 
 
 
     def create_new_records
+        clear_screen
+        prompt = TTY::Prompt.new
+        create_menu = prompt.select("Please select from the following options:",%w(Add_fugitive Add_crime Add_city))
     end 
 
     def delete_records
+        clear_screen
+        prompt = TTY::Prompt.new
+        delete_menu = prompt.select("Please select from the following options:",%w(Delete_fugitive Delete_crime Delete_city))
     end 
 
     # menu = prompt.select("Please select from the following options:",%w(Access_fugitive_records Access_crime_data Update_fugitive_records Update_crime_data )
