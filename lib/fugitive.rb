e9e5d050ae60c0b3d9d88a2de94e3fa667924a8e
Abbie <abbie.coghlan@gmail.com>
2021-01-06 12:32:28 -0600

    fixing accidental delete

--- lib/fugitive.rb
+++ lib/fugitive.rb
@@ -6,6 +6,25 @@ class Fugitive < ActiveRecord::Base
         gets.chomp
     end
    
+    def self.average_age
+        ages = self.select {|fugitive| fugitive.age != nil}.collect {|fugitive| fugitive.age}
+        ages.sum / ages.count
+    end
+
+    def self.oldest_criminal
+        self.select {|fugitive| fugitive.age != nil}.max_by {|fugitive| fugitive.age}
+    end
+
+    def self.youngest_criminal
+        self.select {|fugitive| fugitive.age != nil}.min_by {|fugitive| fugitive.age}
+    end
+
+    # def self.most_common_hair_color
+    #     hair_colors = self.select {|fugitive| fugitive.hair_color}
+    #     hair_colors.max_by {|color| hair_colors.count(color)}
+    # end 
+
+    
     # def self.find_by_city(location)
     #     self.crimes.find_by()
         
